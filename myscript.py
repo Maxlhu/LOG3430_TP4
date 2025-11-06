b2710146e3de0e17d69172c9a209ae0f7e5748b3
Your Name <maxence.lhuisset@polymtl.ca>
2025-11-06 15:04:52 -0500

    fail bisect make fail action

--- myscript.py
+++ myscript.py
@@ -3,14 +3,28 @@ import sys
 
 def main():
     if len(sys.argv) != 3:
-        print("wrong number of arguments")
+        print("Wrong number of arguments")
         sys.exit(1)
 
     bad_hash = sys.argv[1]
     good_hash = sys.argv[2]
 
     os.system(f"git bisect start {bad_hash} {good_hash}")
-    os.system("git bisect run python manage.py test")
+    exit_code = os.system("git bisect run python manage.py test > bisect_log.txt 2>&1")
+
+    with open("bisect_log.txt", "r", encoding="utf-8") as f:
+        log = f.read()
+
+    if "is the first bad commit" in log:
+        bad_line = [line for line in log.splitlines() if "is the first bad commit" in line][0]
+        print(f"Bisect found a bad commit: {bad_line}")
+        sys.exit(1)
+    elif exit_code != 0:
+        print("Bisect failed due to an error in tests or setup.")
+        sys.exit(1)
+    else:
+        print("No bad commit found.")
+        sys.exit(0)
 
 if __name__ == "__main__":
     main()
