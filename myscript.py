0811a77764ebc62a5a6fd8ae66aa1c7e441adba4
Your Name <maxence.lhuisset@polymtl.ca>
2025-11-06 15:01:24 -0500

    minor fix

--- myscript.py
+++ myscript.py
@@ -2,11 +2,15 @@ import os
 import sys
 
 def main():
+    if len(sys.argv) != 3:
+        print("wrong number of arguments")
+        sys.exit(1)
+
     bad_hash = sys.argv[1]
     good_hash = sys.argv[2]
-    os.system("git bisect start {bad_hash} {good_hash}")
-    os.system("git bisect run python manage.py test")
-    
-if __name__ == '__main__':
-    main()
 
+    os.system(f"git bisect start {bad_hash} {good_hash}")
+    os.system("git bisect run python manage.py test")
+
+if __name__ == "__main__":
+    main()
